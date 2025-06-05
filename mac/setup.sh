f946cb6bf916a9d7edf18f6e22a33b63661c63f4
Barbara Stekas <41453265+bstekas@users.noreply.github.com>
2025-06-05 18:57:44 -0400

    Update setup.sh
    add append to zshrc function
    make install of homebrew and conda conditional

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,17 +1,44 @@
-# Install miniconda for python env management
-mkdir -p ~/miniconda3
-curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm ~/miniconda3/miniconda.sh
+#!/bin/sh
 
-source ~/miniconda3/bin/activate
-conda init --all
+# Based on the thoughtbot laptop script!
+
+set -e
+
+append_to_zshrc() {
+  local text="$1" zshrc
+  local skip_new_line="${2:-0}"
+
+  if [ -w "$HOME/.zshrc.local" ]; then
+    zshrc="$HOME/.zshrc.local"
+  else
+    zshrc="$HOME/.zshrc"
+  fi
+
+  if ! grep -Fqs "$text" "$zshrc"; then
+    if [ "$skip_new_line" -eq 1 ]; then
+      printf "%s\\n" "$text" >> "$zshrc"
+    else
+      printf "\\n%s\\n" "$text" >> "$zshrc"
+    fi
+  else
+    echo "\n.zshrc already contains \"$text\"\n" 
+  fi
+}
+
+# Install miniconda for python env management
+conda --version 2> /dev/null || (
+  mkdir -p ~/miniconda3 &&
+  curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh &&
+  bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3 &&
+  rm ~/miniconda3/miniconda.sh &&
+  source ~/miniconda3/bin/activate &&
+  conda init --all)
 
 conda update conda
 conda config --add create_default_packages ipykernel
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew --version 2> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Determine Homebrew prefix
 arch="$(uname -m)"
@@ -21,7 +48,7 @@ else
   HOMEBREW_PREFIX="/usr/local"
 fi
 
-eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+append_to_zshrc "eval \"\$($HOMEBREW_PREFIX/bin/brew shellenv)\""
 
 # Install pipx for global python cli apps
 brew install pipx
